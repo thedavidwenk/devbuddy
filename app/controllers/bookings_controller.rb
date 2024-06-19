7d8410ab3a1a7025ad4a899e5d4e8765b6922b52
Gabriela Cardoso <157655310+gabicards@users.noreply.github.com>
2024-06-19 11:50:19 +0200

    Routes update

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,29 +1,32 @@
 class BookingsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_user
+  # before_action :authenticate_user!
+  # before_action :set_user
 
-  def new
-    @booking = @user.buddy1_bookings.new
+  def index
   end
 
-  def create
-    @booking = @user.buddy1_bookings.new(booking_params)
-    @booking.buddy2 = User.find(params[:booking][:buddy2_id]) # Ensure buddy2 is set correctly
+  # def new
+  #   @booking = @user.buddy1_bookings.new
+  # end
 
-    if @booking.save
-      redirect_to user_bookings_path(@user), notice: 'Booking was successfully created.'
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @booking = @user.buddy1_bookings.new(booking_params)
+  #   @booking.buddy2 = User.find(params[:booking][:buddy2_id]) # Ensure buddy2 is set correctly
 
-  private
+  #   if @booking.save
+  #     redirect_to user_bookings_path(@user), notice: 'Booking was successfully created.'
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  def set_user
-    @user = User.find(params[:user_id])
-  end
+  # private
 
-  def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :note, :buddy2_id)
-  end
+  # def set_user
+  #   @user = User.find(params[:user_id])
+  # end
+
+  # def booking_params
+  #   params.require(:booking).permit(:start_date, :end_date, :note, :buddy2_id)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :users, only: [:index] do
+    resources :bookings, only: [:create]
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  resources :users, only: [:index, :show]
-
-  root to: "users#index"
+  resources :bookings, only: [:index, :edit, :update, :destroy] do
+    resources :feedbacks, only: [:new, :create]
+  end
 end
