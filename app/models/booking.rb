a9ad53fb41160d2150f2bbfd3c1975823d88a78d
Robert Baufeld <rbaufeld@posteo.de>
2024-06-21 11:50:02 +0200

    edited associations for user, booking and time_slot models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,11 +1,11 @@
 class Booking < ApplicationRecord
-  belongs_to :buddy1, class_name: 'User'
-  belongs_to :buddy2, class_name: 'User'
+  belongs_to :user, class_name: 'User'
+  belongs_to :booker, class_name: 'User'
+  belongs_to :time_slot
 
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :buddy1, presence: true
-  validates :buddy2, presence: true
+  validates :user, presence: true
+  validates :booker, presence: true
+  validates :time_slot, presence: true
 
   # With this line it selects the bookings where the date is greater than or equal to today's date.
   # The Upcoming scope is used in the users controller.
--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -1,3 +1,8 @@
 class TimeSlot < ApplicationRecord
   belongs_to :user_id
+
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :day, presence: true
+  validates :user, presence: true
 end
