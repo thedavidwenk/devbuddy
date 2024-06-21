30af0c2f0ea887c571cef9b690a511322465e0ab
David Wenk <davidwenk@icloud.com>
2024-06-21 13:56:04 +0200

    basic search and filter function works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
+  SKILL_LEVELS = ["Beginner", "Intermediate", "Advanced", "Expert"] 
+
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
