8b5ab73337f79222d36d9ba84a1616eea0276214
Robert Baufeld <rbaufeld@posteo.de>
2024-06-21 21:26:12 +0200

    set up the create action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,32 +1,40 @@
 class BookingsController < ApplicationController
-  # before_action :authenticate_user!
-  # before_action :set_user
+  before_action :authenticate_user!, :set_user
 
   def index
+    @bookings = Booking.all
   end
 
-  # def new
-  #   @booking = @user.buddy1_bookings.new
-  # end
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.booker = current_user
+    @booking.user = @user
+    @booking.time_slot = TimeSlot.find(params[:id])
 
-  # def create
-  #   @booking = @user.buddy1_bookings.new(booking_params)
-  #   @booking.buddy2 = User.find(params[:booking][:buddy2_id]) # Ensure buddy2 is set correctly
+    if @booking.save
+      redirect_to profile_user_path(@user), notice: "Booking was successfully created!"
+    # else
+    # render 'user/show', status: :unprocessable_entity
+    end
+  end
 
-  #   if @booking.save
-  #     redirect_to user_bookings_path(@user), notice: 'Booking was successfully created.'
-  #   else
-  #     render :new
-  #   end
-  # end
+  private
 
-  # private
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 
-  # def set_user
-  #   @user = User.find(params[:user_id])
-  # end
-
-  # def booking_params
-  #   params.require(:booking).permit(:start_date, :end_date, :note, :buddy2_id)
-  # end
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :note, :buddy2_id)
+  end
 end
+
+
+# @booking = @user.buddy1_bookings.new(booking_params)
+# @booking.buddy2 = User.find(params[:booking][:buddy2_id]) # Ensure buddy2 is set correctly
+
+# if @booking.save
+#   redirect_to user_bookings_path(@user), notice: 'Booking was successfully created.'
+# else
+#   render :new
+# end
