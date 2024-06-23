d335762f49780c6d8950870fd964d821e752c524
Gabriela Cardoso <157655310+gabicards@users.noreply.github.com>
2024-06-23 21:09:46 +0200

    Added null:false back

--- db/migrate/20240621100538_add_time_slot_referrence_to_booking.rb
+++ db/migrate/20240621100538_add_time_slot_referrence_to_booking.rb
@@ -1,5 +1,5 @@
 class AddTimeSlotReferrenceToBooking < ActiveRecord::Migration[7.1]
   def change
-    add_reference :bookings, :time_slot, foreign_key: true
+    add_reference :bookings, :time_slot, null: false, foreign_key: true
   end
 end
