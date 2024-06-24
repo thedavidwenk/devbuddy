d74d28c8c89780c9329d46b9ef59660c41533063
Robert Baufeld <rbaufeld@posteo.de>
2024-06-24 15:25:38 +0200

    almost there but input target is missing

--- app/javascript/controllers/time_slot_picker_controller.js
+++ app/javascript/controllers/time_slot_picker_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="time-slot-picker"
 export default class extends Controller {
-  static targets = ["slotId", ]
+  static targets = ["slotId", "timeSlotInput"]
 
   connect() {
     console.log("Hello Time Slot")
@@ -11,6 +11,7 @@ export default class extends Controller {
   getSlotId() {
     const slotId = this.slotIdTarget.dataset.slotId
     console.log(slotId)
-    this.timeSlotInputTarget.value = slotId
+    console.log(this.timeSlotInputTarget)
+    // this.timeSlotInputTarget.value = slotId
   }
 }
