28bd3cbdc2648e0599a1304bc1b660037dda008b
Robert Baufeld <rbaufeld@posteo.de>
2024-06-24 19:13:30 +0200

    finding the bug with Mark

--- app/javascript/controllers/time_slot_picker_controller.js
+++ app/javascript/controllers/time_slot_picker_controller.js
@@ -2,16 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="time-slot-picker"
 export default class extends Controller {
-  static targets = ["slotId", "timeSlotInput"]
+  static targets = ["timeSlotInput"]
 
   connect() {
     console.log("Hello Time Slot")
   }
 
-  getSlotId() {
-    const slotId = this.slotIdTarget.dataset.slotId
-    console.log(slotId)
-    console.log(this.timeSlotInputTarget)
-    // this.timeSlotInputTarget.value = slotId
+  selectTimeSlot(event) {
+    console.log(event.currentTarget.dataset.slotId)
+    console.log(this.timeSlotInputTarget.value)
   }
+
+  // getSlotId() {
+  //   const slotId = this.slotIdTarget.dataset.slotId
+  //   console.log("slotId", slotId)
+  //   console.log(this.timeSlotInputTarget)
+  //   // this.timeSlotInputTarget.value = slotId
+  // }
 }
