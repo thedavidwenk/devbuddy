eb0d9feeaaf3a917ffc4ba8810b069309e479f74
David Wenk <davidwenk@icloud.com>
2024-06-25 15:01:16 +0200

    add validation in timeslot model and css adjustments

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -5,4 +5,13 @@ class TimeSlot < ApplicationRecord
   validates :end_time, presence: true
   validates :day, presence: true
   validates :user, presence: true
+  validate :end_time_after_start_time
+
+  private
+
+  def end_time_after_start_time
+    if end_time <= start_time
+      errors.add(:end_time, "must be after the start time")
+    end
+  end
 end
