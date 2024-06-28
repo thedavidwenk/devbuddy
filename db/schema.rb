e85756e7c249c60ad791621de917e012e4473a34
Robert Baufeld <rbaufeld@posteo.de>
2024-06-28 11:52:53 +0200

    changed datatype for start and endtime in time slots to time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_28_094722) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_28_094933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,8 +61,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_28_094722) do
     t.boolean "reserved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "start_time"
-    t.datetime "end_time"
+    t.time "start_time"
+    t.time "end_time"
     t.index ["user_id"], name: "index_time_slots_on_user_id"
   end
 
