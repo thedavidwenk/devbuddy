0b3e0b779b7e21a14936dbe27c1fc1dab49ef919
Robert Baufeld <rbaufeld@posteo.de>
2024-06-30 21:09:19 +0200

    basic functionality

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,4 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js", preload: true
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.6/dist/umd/popper.min.js", preload: true
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.4/lib/assets/compiled/rails-ujs.js", preload: true
-pin "flatpickr" # @4.6.13
+pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.9/dist/flatpickr.js"
