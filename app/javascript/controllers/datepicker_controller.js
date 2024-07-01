1d8705ed359092104249c1218df41f54f69f2137
Robert Baufeld <rbaufeld@posteo.de>
2024-07-01 10:32:25 +0200

    starting with the day bubbles now

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,11 +3,26 @@ import flatpickr from "flatpickr";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+
+
   connect() {
     flatpickr(this.element, {
       enableTime: true,
       dateFormat: "Y-m-d",
-      minDate: "today"
+      minDate: "today",
+      "disable": [
+        function(date) {
+          // return true to disable
+          return (date.getDay() !== 1); /* insert Value from slot.day here */
+        }
+      ],
+      "locale": {
+        "firstDayOfWeek": 1 // start week on Monday
+      }
     });
   }
+
+  // grab slot.day from show page
+  // if disable all days except slot.day on the calender
+
 }
