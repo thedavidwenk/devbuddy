b3ff2217dcc9ac736c4ffb454c63dae3fe741a2d
Gabriela Cardoso <157655310+gabicards@users.noreply.github.com>
2024-07-01 13:43:06 +0200

    Removed Noticed table from schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_28_120123) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_01_114143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,19 +82,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_28_120123) do
     t.datetime "updated_at", null: false
     t.integer "notifications_count"
     t.index ["record_type", "record_id"], name: "index_noticed_events_on_record"
-  end
-
-  create_table "noticed_notifications", force: :cascade do |t|
-    t.string "type"
-    t.bigint "event_id", null: false
-    t.string "recipient_type", null: false
-    t.bigint "recipient_id", null: false
-    t.datetime "read_at", precision: nil
-    t.datetime "seen_at", precision: nil
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_noticed_notifications_on_event_id"
-    t.index ["recipient_type", "recipient_id"], name: "index_noticed_notifications_on_recipient"
   end
 
   create_table "notifications", force: :cascade do |t|
