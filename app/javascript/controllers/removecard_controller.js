6918c96f7a1c40d590e2a60593411d6dd2a0ddc0
David Wenk <davidwenk@icloud.com>
2024-07-01 13:18:01 +0200

    adjust card layout automatically when removing a card

--- app/javascript/controllers/removecard_controller.js
+++ app/javascript/controllers/removecard_controller.js
@@ -1,12 +1,22 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="removecard"
 export default class extends Controller {
   static targets = ["card"];
 
-  toggleRemove(event) {
+  clickRemove(event) {
     event.preventDefault();
     const card = this.cardTarget;
-    card.classList.add("d-none");
+    card.classList.add("remove-card-transition");
+
+    // Wait for the transition to complete before removing the card from the DOM
+    card.addEventListener(
+      "transitionend",
+      () => {
+        // Remove the card container from the DOM
+        card.parentElement.remove();
+      },
+      { once: true }
+    );
   }
 }
