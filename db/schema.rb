675ac6fa6905169eddcd8920392bddb21141aa1c
Gabriela Cardoso <157655310+gabicards@users.noreply.github.com>
2024-07-01 10:28:02 +0200

    Just commiting

--- db/schema.rb
+++ db/schema.rb
@@ -55,6 +55,24 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_28_120123) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.string "favoritable_type", null: false
+    t.bigint "favoritable_id", null: false
+    t.string "favoritor_type", null: false
+    t.bigint "favoritor_id", null: false
+    t.string "scope", default: "favorite", null: false
+    t.boolean "blocked", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blocked"], name: "index_favorites_on_blocked"
+    t.index ["favoritable_id", "favoritable_type"], name: "fk_favoritables"
+    t.index ["favoritable_type", "favoritable_id", "favoritor_type", "favoritor_id", "scope"], name: "uniq_favorites__and_favoritables", unique: true
+    t.index ["favoritable_type", "favoritable_id"], name: "index_favorites_on_favoritable"
+    t.index ["favoritor_id", "favoritor_type"], name: "fk_favorites"
+    t.index ["favoritor_type", "favoritor_id"], name: "index_favorites_on_favoritor"
+    t.index ["scope"], name: "index_favorites_on_scope"
+  end
+
   create_table "noticed_events", force: :cascade do |t|
     t.string "type"
     t.string "record_type"
@@ -114,7 +132,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_28_120123) do
     t.string "experience_level"
     t.text "about_me"
     t.string "programming_languages"
-    t.string "location"
     t.integer "unread_notifications_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
