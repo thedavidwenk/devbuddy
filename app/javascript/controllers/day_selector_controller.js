b3f198643ef8b6a179b6602ef1738779d6ab7b00
Robert Baufeld <rbaufeld@posteo.de>
2024-07-01 15:29:24 +0200

    active state for days and time-slots

--- app/javascript/controllers/day_selector_controller.js
+++ app/javascript/controllers/day_selector_controller.js
@@ -2,13 +2,28 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="day-selector"
 export default class extends Controller {
-  static targets = ["bubble"];
+  static targets = ["bubble", "slot"];
 
   connect() {
   }
 
-  selectDay() {
-    const bubble = this.bubbleTarget;
-    bubble.classList.toggle("active")
+  selectDay(event) {
+    const clickedBubble = event.currentTarget
+    const bubbles = this.bubbleTargets;
+
+    bubbles.forEach(bubble => {
+      bubble.classList.remove("active")
+    })
+    clickedBubble.classList.toggle("active")
+  }
+
+  selectTime(event) {
+    const clickedSlot = event.currentTarget
+    const slots = this.slotTargets;
+
+    slots.forEach(slot => {
+      slot.classList.remove("active")
+    })
+    clickedSlot.classList.toggle("active")
   }
 }
