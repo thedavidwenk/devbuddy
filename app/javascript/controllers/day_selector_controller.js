4e22271478d6d2c8c781009993bafddba67386ca
Robert Baufeld <rbaufeld@posteo.de>
2024-07-02 09:16:07 +0200

    trying to make days display timeslots

--- app/javascript/controllers/day_selector_controller.js
+++ app/javascript/controllers/day_selector_controller.js
@@ -2,28 +2,29 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="day-selector"
 export default class extends Controller {
-  static targets = ["bubble", "slot"];
-
-  connect() {
-  }
+  static targets = ["bubble", "slot", "slots"];
 
   selectDay(event) {
-    const clickedBubble = event.currentTarget
+    const clickedBubble = event.currentTarget;
     const bubbles = this.bubbleTargets;
 
     bubbles.forEach(bubble => {
-      bubble.classList.remove("active")
+      bubble.classList.remove("active-btn");
     })
-    clickedBubble.classList.toggle("active")
+    clickedBubble.classList.toggle("active-btn");
+
+    
+
+    // this.slotsTargets.classList.remove("d-none")
   }
 
   selectTime(event) {
-    const clickedSlot = event.currentTarget
+    const clickedSlot = event.currentTarget;
     const slots = this.slotTargets;
 
     slots.forEach(slot => {
-      slot.classList.remove("active")
+      slot.classList.remove("active-btn");
     })
-    clickedSlot.classList.toggle("active")
+    clickedSlot.classList.toggle("active-btn");
   }
 }
