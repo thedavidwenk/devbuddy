6047d8cfb5d5de96eac12a14558d302992b1538c
Robert Baufeld <rbaufeld@posteo.de>
2024-08-11 16:55:22 +0200

    added tests to user controller

--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -1,12 +1,7 @@
 require "test_helper"
 
 class NotificationsControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get notifications_index_url
-  #   assert_response :success
-  # end
-
-  # test "should get mark_as_read" do
+    # test "should get mark_as_read" do
   #   get notifications_mark_as_read_url
   #   assert_response :success
   # end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get home" do
+    get root_url
+    assert_response :success
+  end
+
+  test "user should login before making a booking" do
+    post user_bookings_path(users(:one))
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
 end
