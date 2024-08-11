6333b43368cadbd97508c872e3a9d90a8ccdadca
Robert Baufeld <rbaufeld@posteo.de>
2024-08-11 19:22:06 +0200

    added some tests for time slots

--- test/controllers/time_slots_controller_test.rb
+++ test/controllers/time_slots_controller_test.rb
@@ -1,23 +1,33 @@
 require "test_helper"
 
 class TimeSlotsControllerTest < ActionDispatch::IntegrationTest
-  # test "should get new" do
-  #   get time_slots_new_url
-  #   assert_response :success
-  # end
+  setup do
+    @user = users(:one)
+    @valid_time_slot_params = {
+      start_time: "09:00",
+      end_time: "10:00",
+      date: "2024-08-11"
+    }
+  end
 
-  # test "should get create" do
-  #   get time_slots_create_url
-  #   assert_response :success
-  # end
+  test "should create time slot and redirect to account overview" do
+    sign_in @user
 
-  # test "should get edit" do
-  #   get time_slots_edit_url
-  #   assert_response :success
-  # end
+    assert_difference('TimeSlot.count', 1) do
+      post time_slots_path, params: @valid_time_slot_params
+    end
 
-  # test "should get delete" do
-  #   get time_slots_delete_url
-  #   assert_response :success
-  # end
+    assert_redirected_to account_overview_user_path(@user)
+    follow_redirect!
+  end
+
+  test "should not create time slot with invalid data" do
+    sign_in @user
+
+    assert_no_difference('TimeSlot.count') do
+      post time_slots_path, params: { start_time: "09:00", end_time: "08:00", date: "2024-08-11" }
+    end
+
+    assert_response :unprocessable_entity
+  end
 end
